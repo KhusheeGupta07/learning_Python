86607b815e9c24fcbe191b0cd794b5946376a3a5
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-06-02 20:35:31 +0530

    Add files via upload

---
+++ f4.py
@@ -0,0 +1,24 @@
+# a="h"
+# print(ord(a))
+
+# d="H"
+# print(ord(d))
+
+# e="3"
+# print(ord(e))
+
+# w="@"
+# print(ord(w))
+
+# s="college"
+# print(s)
+
+# print(s[1])
+# print(s[-2])
+
+# STRING_SLICING
+STR='GANESHA'
+print(STR[3:8:1])
+print(STR[: :])
+
+
