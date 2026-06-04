bdf2d0cf731df2fea742fcaeeae644a7e58e27b7
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-06-04 22:25:07 +0530

    Add files via upload

---
+++ f6.py
@@ -0,0 +1,25 @@
+# if(23>2):
+#     print("jai shee ram")
+
+# if 51==51:
+#     print("jai shee ram")
+
+# age = int(input("enter your age:"))              #IF_ELSE LADDER
+# if age>18:
+#     print("can vote👍")
+# else:
+#     print("not vote")
+
+
+
+# money= int(input("enter money"))              #IF_ELIF LADDER
+# if money==10:
+#     print("radhe_radhe")
+# elif money==50:   
+#     print("jai shree krishna")
+# elif money==100:   
+#     print("radhe krishna")
+# elif money==500:  
+#     print("radhe _krishna")   
+# else: 
+#     print("ram ram")
