20619900a7c12ae0c296f0700749e1d8eb933da3
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-06-06 22:54:16 +0530

    Add files via upload

---
+++ folder_2/f1_functions.py
@@ -0,0 +1,20 @@
+# def void():
+#     print("namaste_ganesha")
+# void()    
+
+# def add(a,b):
+#     print(a+b)
+# add(1,5)    
+
+# def palindrome_checkar(n):   #palindrome_checkar
+#     c=n
+#     ans=0
+#     while n>0:
+#         ans=ans*10+n%10
+#         n=n//10
+#     if c==ans: 
+#         print("palindrome")
+#     else:
+#         print("not")
+# num = int(input("enter the numbet")) 
+# palindrome_checkar(num)    
