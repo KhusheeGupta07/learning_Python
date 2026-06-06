efc6b6d9001bd415b77b0228fd67df41d289d388
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-06-06 22:54:38 +0530

    Add files via upload

---
+++ folder_2/f2_typ_arguemet.py
@@ -0,0 +1,15 @@
+#POSITIONAL,DEFAULT,KEYWORD
+
+# def mul(a,b,c):
+#     print(a*b*c)   #POSITIONAL  arguement
+# mul(1,5,9)    
+
+
+# def mul(a,b,c=6):
+#     print(a*b*c)   #DEFAULT  arguement
+# mul(1,5,9) 
+
+
+# def mul(a,b):
+#     print(b-a)   #KEYWORD  arguement
+# mul(b=1,a=5) 
