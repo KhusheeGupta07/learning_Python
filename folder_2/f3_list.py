9bb3b239d8d00ad8b257d0ab241971d37f84c94d
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-06-07 22:59:43 +0530

    Add files via upload

---
+++ folder_2/f3_list.py
@@ -0,0 +1,56 @@
+# a=[1,2,3,4,5]
+# print(a)
+# print(type(a))  #ordered nature
+# print(a[-1])
+# print(a[2])
+
+# l=[3,5,2,31,21]      #mutable nature
+# l[2]=11
+# print(l)
+
+# m=[1,1,1,2,2]      #duplicate nature
+
+# # ACESSING STRINGS
+# FRIUTS= ["apple","banana","asdfg"]
+# print(FRIUTS[1])
+# print(FRIUTS[0:2])
+
+# a=[1,2,3,4,5]
+# #BY TRAVERSING ON VALEUSS
+# # for i in a:
+# #     print(i)
+
+#   #BY TRAVERSING ON INDEX 
+# for i  in range(0,len(a)):
+
+#     print(i)  
+#     print(f"{[i]} :{a[i]}")
+
+
+# APPEND
+
+
+# a=[1,2,3,4,5]
+# a.append(7)
+# print(a)
+# a.append("hello")
+# print(a)
+# a.append(True)
+# print(a)
+
+
+# #INSERT
+# a=[1,2,4,5]
+# a.insert(2,30)
+# print(a)
+
+
+#POP
+a=[1,2,3,4,5,77]
+# b=a.pop()
+# print(a)
+# print(b)
+
+c=a.pop(2)
+print(a)
+print(c)
