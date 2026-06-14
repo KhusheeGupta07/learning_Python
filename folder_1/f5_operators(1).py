1660d0a6899998d6a4c81834356020c6de165d19
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-06-14 21:54:40 +0530

    Add files via upload

---
+++ folder_1/f5_operators(1).py
@@ -0,0 +1,33 @@
+##
+# name="ram"
+# age="25"
+# print(f"hi my name is {name} and my age is {age}")         #AUTOMATIC STRING
+# print("hi my name is ",name,"and my age is",age)           #MULTIPLE STRING
+
+# #INPUT
+# input("what is your age:-")           # YHA WE CAN TAKE INPUT ON TERMINAL FROM KEYBOARD
+# age=input("what is your age:-")
+# print(type(age))
+# print(f"your age is {age}")
+
+# umar=int (input("what is your age:-"))
+# print(type(umar))
+# print(f"your age is {umar+1}")
+
+
+#ARITHMATIC OPERATORS
+
+# a=1
+# m=31
+# print(a+m+21-2)
+
+# a=13
+# print(a/2)
+# print(int(a/2))
+# print(a//2)
+
+# print(6**2)
+# print(37%5)
+
+print(10 / 2 *5)
+print(10 * 2/ 5)
