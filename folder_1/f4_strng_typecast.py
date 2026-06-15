e78c5588bb02d90852fc9d687f77270f44b1ffa2
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-06-15 23:10:29 +0530

    Add files via upload

---
+++ folder_1/f4_strng_typecast.py
@@ -0,0 +1,47 @@
+# a="h"
+# print(ord(a))
+
+# d="H"
+# print(ord(d))
+
+# e="3"
+# print(ord(e))
+
+# w="@"
+# print(ord(w))
+
+# s="college"
+# print(s)
+
+# print(s[1])
+# print(s[-2])
+
+# STRING_SLICING
+
+
+# STR='GANESHA'
+# print(STR[3:8:1])
+# print(STR[: :])
+
+
+#TYPE CASTING
+# ab='23'
+# ab=float(ab)
+# print(type(ab))
+
+# a=1
+# a=float(a)
+# print(type(a))
+
+a=12
+b=0
+c=12.4
+d=0.0
+e=""
+f="hello"
+print(bool(a))
+print(bool(b))
+print(bool(c))
+print(bool(d))
+print(bool(e))
+print(bool(f))
