2c27e5806c34635ec59bf68b9cc6d69ff77615f5
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-06-16 23:01:26 +0530

    Add files via upload

---
+++ folder_1/f5operators(2).py
@@ -0,0 +1,23 @@
+# COMPARISON_OPERATORS
+# print(14 == 12)  #GIVE ANSWER IN BOOLEAN IE,TRUE OR FALSE
+# print(13 ==13)
+# print(15 !=15)
+
+
+#LOGICAL_OPERATORS
+# print(11>2 and 13==13 and 16!=1)   #AND OPERATION
+# print(11<2 or 13==78 or 16!=1)   #OR OPERATION
+# print( not 13==1)    #NOT OPERATION
+
+
+#ASSIGNMENT OPERATORS
+# {+=,-=,*=,/=,//=,**=}
+
+
+
+#SOME_EXAMPLES
+print("Hello"<"hello")     #AS IN PYTHON ORDINARY VALUE OF H(72) IS LESS THSN h(104)
+print("hello"<"hello")
+print("Hello"== "Hello")
+print("Hello">"hello")
+print("Hello"=="hello")
