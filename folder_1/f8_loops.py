a57dd12cc7205c97f458e1e3afe886f84b0c86eb
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-06-19 23:11:32 +0530

    Add files via upload

---
+++ folder_1/f8_loops.py
@@ -0,0 +1,48 @@
+# range(1,11,1)
+# range(10,101,10)
+# range(22)
+
+# for i in range(10,101,10):
+#     print(i)
+
+# for i in range(6):
+#     print(i)
+
+# n=int(input("enter no."))
+# for i in range(n,(n*10)+1,n):
+#     print(i)
+
+# a="students"
+# for i in a:
+#     print(i)
+
+# for i in range(0,len(a),1):
+#     print(f"{i}: {a[i]}")
+    # print(i)
+    # print(a[i])
+
+#                                 USE OF BREAK;CONTINUE;ELSE
+# for i in range(1,11):
+#     if i==4:
+#      break
+#     print(i) 
+
+# for i in range(1,11):
+#     if i==4 or i==9:
+#      continue
+#     print(i)  
+
+# for i in range(1,11):
+#     if i==4 or i==5 or i==6:
+#      continue
+#     print(i)        
+
+
+# for i in range(1,11):
+#     if i==45:
+#      break
+#     print(i)
+# else:
+#    print("break was not used")     
+
+print(ord("Z"))
