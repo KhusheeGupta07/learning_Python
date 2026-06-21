d14c326ffc603785e8498995f01ff8a5b860faa4
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-06-21 23:02:13 +0530

    Add files via upload

---
+++ folder_2/f5_set.py
@@ -0,0 +1,30 @@
+# #DICTIONARY
+# a={}
+# print(type(a))
+
+# #SET
+# a= {1.2,3,21}
+# print(type(a))
+
+
+# l=[1,1,1,2,2,2,3,3]
+# s=set(l)
+# print(s)
+
+
+# a="hello"             #PRINTING HASH_VALUE
+# print(hash(a))  
+
+# s={123,"hello","gannu",(1,2,3),{123}}   #LIST CAN'T BE ACESSED AS SET ELEMENT AS IT IS NOT HASHABLE
+# print(s[1])
+
+#s={123,"hello","gannu",(1,2,3)}    #HERE,WITHOUT LIST, THERE IS NO ERROR
+
+# a={1,21,13,411,5}
+# # for i in range(len(a)):
+# #     print(a[i])                     #'set' object is not subscriptable
+
+
+# for i in a:
+#     print(i)                         #'set' object is not in ordered form
+
