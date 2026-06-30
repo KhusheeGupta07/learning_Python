acdf00d7a7f2bf7077e06d1a7e969797d81dad48
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-06-30 21:57:24 +0530

    Add files via upload

--- folder_2/f9_FILE_HANDELING.py
+++ folder_2/f9_FILE_HANDELING.py
@@ -1,8 +1,28 @@
-# open("handmade.txt","x")
+# open("handmade.txt","x")            #CREATING A FILE           
 
-f=open("handmade.txt","w")
-data= input("write your thoughts")
-f.write(data)
+# f=open("handmade.txt","w")            #WRITING INSIDE A FILE
+# data= input("write your thoughts")
+# f.write(data)
 
-# file = open("f3_list.py","r")k
+
+
+# file = open("handmade.txt","r")           #READING INSIDE A FILE
 # print(file.read())
+
+
+
+# file = open("python_2/f3_list.py","r")     
+
+# #F3 WALI FILE KO READ KRNE K LIYE PHLE USE PYTHON 2 SE NIKALKR PYHON DIRECTORY M SHIFT KRO,, 
+# # 
+# # ydi ""python_2/"" yeh ni laga rhe ho toh
+
+# print(file.read())
+
+# f=open("handmade.txt","a")
+# dataa = input("nyer the content u wnat:--")           #APPEND
+# print(f.write(dataa))
+
+#USE OF WITH OPEN_KEYWORD
+with open("handmade.txt","a") as f:
+    f.write("" +"MY NAME IS HARI")
