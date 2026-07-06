8d820cafa8169cf4188eb31d489c35233938bf1f
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-07-06 21:51:23 +0530

    Add files via upload

---
+++ folder_3/f1_clases.py
@@ -0,0 +1,11 @@
+# class car:
+#     print("hare ram hare ram,,, RAMA RAMA HARE HARE, HARE KRISHNAAA ,HARE KRISHNAAA,KRISHNAAA KRISHNAAA HARE HARE !!")
+
+class car:
+    brand="bmw"  #attributes
+     
+    def sound():    #method
+        print("expensive but luxuxry and royal,, hare krishna")
+
+print(car.brand)
+car.sound()        
