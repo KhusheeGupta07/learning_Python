fec578cbebdc1fdb6f739280475b08209163d163
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-07-07 22:20:45 +0530

    Add files via upload

---
+++ folder_3/f2_objects.py
@@ -0,0 +1,15 @@
+class car:
+    brand="bmw"  #attributes
+     
+    def sound():    #method
+        print("hare krishna")
+
+rebok = car() 
+campus= car()  
+print(rebok.brand)     
+print(campus.brand) 
+
+# rebok.sound()
+# campus.sound()          #YE DONO ONLY TBH CHAL RAHE H JAB METHOD-- SOUND M SELF LIKHA HO BUT
+#                         #  YE NEECHE, SIMPLE WALA SELF K SATH NI CHAL RHA 
+car.sound()
