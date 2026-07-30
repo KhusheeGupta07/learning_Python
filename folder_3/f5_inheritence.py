b23b0408afc8069396e6a0788fc2351c3b5ea641
KhusheeGupta07 <125104049@nitkkr.ac.in>
2026-07-30 21:39:08 +0530

    Add files via upload

---
+++ folder_3/f5_inheritence.py
@@ -0,0 +1,9 @@
+#like in function we  take parameters, here in those parameters will be classes.
+
+class Animal:   #parent class
+    def __init__(self):
+        pass
+    pass
+
+class Humans(Animal):   #child  class
+    pass
